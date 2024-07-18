2b584bd9d2c430d1ecd94d480a5ac32f1d4dcee0
Tobias Briones <tbriones@mathswe.com>
2024-07-18 12:07:33 -0600

    Integrate download integrity check
    It requires an integrity check to verify after downloading files, and it will add more methods progressively, as well as deprecating the "None" variant when other methods get implemented.

--- system/src/download/download.rs
+++ system/src/download/download.rs
@@ -6,18 +6,35 @@ use std::error::Error;
 use std::fs::File;
 use std::io;
 use std::io::{ErrorKind, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use reqwest::blocking;
+use crate::download::hashing::Hash;
+
+#[derive(Debug)]
+pub enum Integrity {
+    Hash(Hash),
+    None,
+}
+
+impl Integrity {
+    pub fn check(&self, file_path: &Path) -> io::Result<bool> {
+        match self {
+            Integrity::Hash(hash) => hash.matches(file_path),
+            Integrity::None => Ok(true),
+        }
+    }
+}
 
 pub struct DownloadRequest {
     url: String,
+    integrity: Integrity,
 }
 
 impl DownloadRequest {
-    pub fn new(url: String) -> io::Result<Self> {
+    pub fn new(url: String, integrity: Integrity) -> io::Result<Self> {
         if url.starts_with("https://") {
-            Ok(DownloadRequest { url })
+            Ok(DownloadRequest { url, integrity })
         } else {
             Err(io::Error::new(ErrorKind::Other, format!("URL {} is not HTTPS protocol", url)))
         }
@@ -64,6 +81,19 @@ impl Downloader {
                     .and_then(|mut file| file.write_all(&bytes))
                     .map_err(|err| io_err(format!("Failed to write {}: {}", filename, err)))
             })
+            .and_then(|_| {
+                self
+                    .req
+                    .integrity
+                    .check(self.path.as_path())
+                    .and_then(|check| {
+                        if check {
+                            Ok(())
+                        } else {
+                            Err(io_err(format!("Downloaded file {} failed {:?} integrity check", filename, self.req.integrity)))
+                        }
+                    })
+            })
     }
 }
 
@@ -71,21 +101,27 @@ impl Downloader {
 mod tests {
     use std::fs;
     use std::path::Path;
-
+    use crate::download::hashing::HashAlgorithm;
     use crate::tmp::tmp::TmpWorkingDir;
 
     use super::*;
 
     #[test]
     fn checks_url() {
-        let req = DownloadRequest::new("https://example.com".to_string());
+        let req = DownloadRequest::new(
+            "https://example.com".to_string(),
+            Integrity::None,
+        );
 
         assert!(req.is_ok());
     }
 
     #[test]
     fn rejects_unsafe_url() {
-        let req = DownloadRequest::new("http://example.com".to_string());
+        let req = DownloadRequest::new(
+            "http://example.com".to_string(),
+            Integrity::None,
+        );
 
         assert!(req.is_err());
     }
@@ -97,7 +133,9 @@ mod tests {
         let url = format!("{}/system/resources/test/download/{}", base_url, filename);
         let temp_dir = TmpWorkingDir::new()?;
         let temp_file_path = temp_dir.join(filename.as_ref());
-        let req = DownloadRequest::new(url)?;
+        let checksum = "0ecfebe350c45dbded8cfb32d3af0b910bde66fc2aafbafabdaaeef6cae48a59".to_string();
+        let integrity = Integrity::Hash(Hash::new(HashAlgorithm::Sha256, checksum));
+        let req = DownloadRequest::new(url, integrity)?;
         let downloader = Downloader::new(req, temp_file_path.clone());
 
         downloader.download_blocking(filename)?;
@@ -123,7 +161,7 @@ mod tests {
         let url = format!("{}/system/resources/test/download/{}", base_url, filename);
         let temp_dir = TmpWorkingDir::new()?;
         let temp_file_path = temp_dir.join(filename.as_ref());
-        let req = DownloadRequest::new(url)?;
+        let req = DownloadRequest::new(url, Integrity::None)?;
         let downloader = Downloader::new(req, temp_file_path.clone());
 
         let res = downloader.download_blocking(filename);
