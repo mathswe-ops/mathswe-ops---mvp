efe603635dcafd8d7a1a87c5272998f9faf077b4
Tobias Briones <tbriones@mathswe.com>
2024-07-17 18:11:58 -0600

    Add struct DownloadRequest with URL checks
    It defines a download model to ensure its safety, protocols, or rules.

--- system/src/download/download.rs
+++ system/src/download/download.rs
@@ -10,25 +10,42 @@ use std::path::PathBuf;
 
 use reqwest::blocking;
 
+pub struct DownloadRequest {
+    url: String,
+}
+
+impl DownloadRequest {
+    pub fn new(url: String) -> io::Result<Self> {
+        if url.starts_with("https://") {
+            Ok(DownloadRequest { url })
+        } else {
+            Err(io::Error::new(ErrorKind::Other, format!("URL {} is not HTTPS protocol", url)))
+        }
+    }
+}
+
 pub struct Downloader {
+    req: DownloadRequest,
     path: PathBuf,
 }
 
 impl Downloader {
-    pub fn new(path: PathBuf) -> Self {
-        Downloader { path }
+    pub fn new(req: DownloadRequest, path: PathBuf) -> Self {
+        Downloader { req, path }
     }
 
-    pub fn to_file(self) -> io::Result<File> {
-        File::create_new(self.path)
+    pub fn to_file(&self) -> io::Result<File> {
+        File::create_new(&self.path)
     }
 
-    pub fn download_blocking(self, filename: &str, url: &str) -> io::Result<()> {
+    pub fn download_blocking(&self, filename: &str) -> io::Result<()> {
         let format_err_msg = |msg: String, target: String| { format!("{}: {}", msg, target) };
 
         let io_err = |msg: String| { io::Error::new(ErrorKind::Other, msg) };
 
         let to_io_err = |msg: String| |err: reqwest::Error| io_err(format_err_msg(msg, err.to_string()));
+
+        let url = &self.req.url;
 
         blocking::get(url)
             .map_err(to_io_err(format!("Failed to fetch {}", url)))
@@ -60,15 +77,30 @@ mod tests {
     use super::*;
 
     #[test]
+    fn checks_url() {
+        let req = DownloadRequest::new("https://example.com".to_string());
+
+        assert!(req.is_ok());
+    }
+
+    #[test]
+    fn rejects_unsafe_url() {
+        let req = DownloadRequest::new("http://example.com".to_string());
+
+        assert!(req.is_err());
+    }
+
+    #[test]
     fn downloads_file() -> io::Result<()> {
         let base_url = "https://raw.githubusercontent.com/mathswe-ops/mathswe-ops---mvp/main";
         let filename = "test_file.txt";
         let url = format!("{}/system/resources/test/download/{}", base_url, filename);
         let temp_dir = TmpWorkingDir::new()?;
         let temp_file_path = temp_dir.join(filename.as_ref());
-        let downloader = Downloader::new(temp_file_path.clone());
+        let req = DownloadRequest::new(url)?;
+        let downloader = Downloader::new(req, temp_file_path.clone());
 
-        downloader.download_blocking(filename, &url)?;
+        downloader.download_blocking(filename)?;
         assert!(temp_file_path.exists());
 
         let test_file_path = Path::new("resources")
@@ -91,9 +123,10 @@ mod tests {
         let url = format!("{}/system/resources/test/download/{}", base_url, filename);
         let temp_dir = TmpWorkingDir::new()?;
         let temp_file_path = temp_dir.join(filename.as_ref());
-        let downloader = Downloader::new(temp_file_path.clone());
+        let req = DownloadRequest::new(url)?;
+        let downloader = Downloader::new(req, temp_file_path.clone());
 
-        let res = downloader.download_blocking(filename, &url);
+        let res = downloader.download_blocking(filename);
 
         match res {
             Ok(_) => { panic!("It could download non-existent file!") }
