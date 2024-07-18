6f734d9e9ce8f9b63aa3b090b80db8db231f4344
Tobias Briones <tbriones@mathswe.com>
2024-07-17 21:33:52 -0600

    Define hashing API with types HashAlgorithm, Hash

--- system/src/download/hashing.rs
+++ system/src/download/hashing.rs
@@ -10,7 +10,33 @@ use std::str::FromStr;
 
 use sha2::{Digest, Sha256};
 
-pub(crate) fn calculate_sha256(file_path: &Path) -> io::Result<String> {
+pub(crate) enum HashAlgorithm {
+    Sha256
+}
+
+pub(crate) struct Hash {
+    algorithm: HashAlgorithm,
+    hash: String,
+}
+
+impl Hash {
+    pub(crate) fn new(algorithm: HashAlgorithm, hash: String) -> Self {
+        Hash { algorithm, hash }
+    }
+
+    pub(crate) fn matches(&self, file_path: &Path) -> io::Result<bool> {
+        self.calculate_hash(file_path)
+            .map(|file_hash| self.hash == file_hash)
+    }
+
+    fn calculate_hash(&self, file_path: &Path) -> io::Result<String> {
+        match self.algorithm {
+            HashAlgorithm::Sha256 => calculate_sha256(file_path)
+        }
+    }
+}
+
+fn calculate_sha256(file_path: &Path) -> io::Result<String> {
     let file = File::open(file_path)?;
     let mut reader = BufReader::new(file);
     let mut hasher = Sha256::new();
@@ -35,10 +61,25 @@ mod tests {
     use std::io;
     use std::path::Path;
 
-    use crate::download::hashing::calculate_sha256;
+    use crate::download::hashing::{calculate_sha256, Hash, HashAlgorithm};
 
     #[test]
     fn checks_sample_file_sha256() -> io::Result<()> {
+        let checksum = "0ecfebe350c45dbded8cfb32d3af0b910bde66fc2aafbafabdaaeef6cae48a59";
+        let hash = Hash::new(HashAlgorithm::Sha256, checksum.to_string());
+        let test_file_path = Path::new("resources")
+            .join("test")
+            .join("download")
+            .join("test_file.txt");
+        let check = hash.matches(&test_file_path)?;
+
+        assert_eq!(true, check);
+
+        Ok(())
+    }
+
+    #[test]
+    fn checks_sample_file_sha256_impl() -> io::Result<()> {
         let checksum = "0ecfebe350c45dbded8cfb32d3af0b910bde66fc2aafbafabdaaeef6cae48a59";
         let test_file_path = Path::new("resources")
             .join("test")
