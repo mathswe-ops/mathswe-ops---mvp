aaac0c3cb53a8e3559046c33fd6ace94d42c0907
Tobias Briones <tbriones@mathswe.com>
2024-07-19 21:01:46 -0600

    Redesign struct CmdError with variants Io, UnsuccessfulStatus
    This implementation handles successful program calls that terminated with unsuccessful status code, thus, in the end, the program call gives an error to the caller. It also removes remarkable redundancy from the ADTs.

--- system/src/cmd.rs
+++ system/src/cmd.rs
@@ -6,33 +6,38 @@ use std::fmt::{Display, Formatter};
 use std::io::Error;
 use std::process::{Child, Command, Output};
 
-use CmdErrorCause::{StartFail, WaitFail};
+use CmdErrorCause::UnsuccessfulStatus;
 
-#[derive(Debug, Clone, Copy)]
-pub enum CmdErrorCause { StartFail, WaitFail }
+use crate::cmd::CmdErrorCause::Io;
+use crate::cmd::IoErrorCause::{StartFail, WaitFail};
+
+#[derive(Debug)]
+pub enum IoErrorCause { StartFail, WaitFail }
+
+#[derive(Debug)]
+pub enum CmdErrorCause { Io(IoErrorCause, Error), UnsuccessfulStatus(Option<i32>) }
 
 #[derive(Debug)]
 pub struct CmdError {
     cmd: String,
     cause: CmdErrorCause,
-    io_cause: Error,
 }
 
 impl CmdError {
-    fn from(cmd: &str, cause: CmdErrorCause) -> impl Fn(Error) -> CmdError + '_ {
-        move |io_cause: Error| CmdError {
+    fn from(cmd: &str, cause: CmdErrorCause) -> CmdError {
+        CmdError {
             cmd: cmd.to_string(),
             cause,
-            io_cause,
         }
     }
 }
 
 impl Display for CmdError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let reason = match self.cause {
-            StartFail => format!("Fail to start command {}. \nCause: {}", self.cmd, self.io_cause),
-            WaitFail => format!("Fail to wait for command {} exit. \nCause: {}", self.cmd, self.io_cause),
+        let reason = match &self.cause {
+            Io(StartFail, err) => format!("Fail to start command {}. \nCause: {}", self.cmd, err),
+            Io(WaitFail, err) => format!("Fail to wait for command {} exit. \nCause: {}", self.cmd, err),
+            UnsuccessfulStatus(code) => format!("Unsuccessful command {} execution. \nCause: Status code {:?}", self.cmd, code),
         };
 
         write!(f, "{}", reason)
@@ -42,17 +47,26 @@ impl Display for CmdError {
 pub type Result<T> = std::result::Result<T, CmdError>;
 
 pub fn exec_cmd(cmd: &str, args: &[&str]) -> Result<Output> {
+    let io_err = move |cause: IoErrorCause| move |err: Error| CmdError::from(cmd, Io(cause, err));
     let err = |cause: CmdErrorCause| CmdError::from(cmd, cause);
+    let check_success = |output: Output| {
+        if output.status.success() {
+            Ok(output)
+        } else {
+            Err(err(UnsuccessfulStatus(output.status.code())))
+        }
+    };
     let wait_child = |child: Child| {
         child
             .wait_with_output()
-            .map_err(err(WaitFail))
+            .map_err(io_err(WaitFail))
+            .and_then(check_success)
     };
 
     Command::new(cmd)
         .args(args)
         .spawn()
-        .map_err(err(StartFail))
+        .map_err(io_err(StartFail))
         .and_then(wait_child)
 }
 
@@ -78,12 +92,15 @@ mod tests {
         println!("{}", format!("Command {} {:?}", cmd, args));
 
         match exec_cmd(cmd, args) {
-            Ok(output) => {
-                assert_eq!(Some(code), output.status.code());
-                println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-                eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+            Ok(_) => panic!("Expected command to fail with unsuccessful status code, but it succeeded."),
+            Err(e) => {
+                let status_code_matches = match e.cause {
+                    UnsuccessfulStatus(Some(actual)) => actual == code,
+                    _ => false
+                };
+
+                assert!(status_code_matches);
             }
-            Err(e) => panic!("Command failed: {}", e),
         }
         println!();
     }
