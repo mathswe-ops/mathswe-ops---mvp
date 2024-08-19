05b058f6a09c7b1a763a150d2512e0e4f800bc98
Tobias Briones <tbriones@mathswe.com>
2024-08-19 13:24:11 -0600

    Refactor app name to denote it is the web app
    The doted name denotes we're talking about the web app in the footer notes.

--- ops.math.software---mvp/src/info.tsx
+++ ops.math.software---mvp/src/info.tsx
@@ -3,6 +3,6 @@
 // This file is part of https://github.com/mathswe-ops/mathswe-ops---mvp
 
 export const APP_SEMVER = "0.1.0-dev";
-export const APP_NAME = "MathSwe Ops";
+export const APP_NAME = "Ops.Math.Software";
 export const APP_VERSION = `MVP v${ APP_SEMVER }`;
 export const APP_VERSION_LABEL = `${ APP_NAME } ${ APP_VERSION }`;
