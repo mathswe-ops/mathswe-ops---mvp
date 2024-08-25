a3d5e08a05b1ad09e68172f144ec84510ed54856
Tobias Briones <tbriones@mathswe.com>
2024-08-25 00:12:53 -0600

    Implement mod batch with structs BatchReport, BatchOperation

--- system/src/main/cli.rs
+++ system/src/main/cli.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // This file is part of https://github.com/mathswe-ops/mathswe-ops---mvp
 
-use crate::image::{ImageId};
+use crate::main::batch::BatchOperation;
 use crate::main::cli::CliCommand::{Install, Reinstall, Uninstall};
 use crate::main::exec::{OperationContext, OperationExecution};
 use crate::main::system::Operation;
@@ -43,85 +43,17 @@ impl CliCommand {
     pub fn execute(&self) -> Result<(), String> {
         let ctx = OperationContext::load()?;
         let exec = OperationExecution { ctx };
+        let batch = BatchOperation { operation: self.to_operation() };
 
         match self {
             Install { images } =>
-                self.execute_batch(images, |id_raw| exec.install(id_raw)),
+                batch.execute(images, |id_raw| exec.install(id_raw)),
 
             Uninstall { images } =>
-                self.execute_batch(images, |id_raw| exec.uninstall(id_raw)),
+                batch.execute(images, |id_raw| exec.uninstall(id_raw)),
 
             Reinstall { images } =>
-                self.execute_batch(images, |id_raw| exec.reinstall(id_raw)),
-        }
-    }
-
-    fn execute_batch(
-        &self,
-        images: &Vec<String>,
-        exec: impl Fn(&String) -> Result<ImageId, String>,
-    ) -> Result<(), String> {
-        // 0: Number of Ok results, 1: List of IDs that failed
-        let empty_report = (0, Vec::new());
-
-        let report = images
-            .iter()
-            .map(exec)
-            .fold(empty_report, Self::success_fail_report);
-
-        self.print_batch_report(report)
-    }
-
-    fn batch_report_msg(&self, (ok_num, err_ids): (i32, Vec<String>)) -> String {
-        match self.to_operation() {
-            Operation::Install => format!("{} images successfully installed; {} images failed to install.", ok_num, err_ids.len()),
-            Operation::Uninstall => format!("{} images successfully uninstalled; {} images failed to uninstall.", ok_num, err_ids.len()),
-            Operation::Reinstall => format!("{} images successfully reinstalled; {} images failed to reinstall.", ok_num, err_ids.len()),
-        }
-    }
-
-    fn batch_report_success_msg(&self, ok_num: i32) -> String {
-        let plural = if ok_num > 1 { "s" } else { "" };
-
-        match self.to_operation() {
-            Operation::Install => format!("✅ Install {} image{}.", ok_num, plural),
-            Operation::Uninstall => format!("✅ Uninstall {} image{}.", ok_num, plural),
-            Operation::Reinstall => format!("✅ Reinstall {} image{}.", ok_num, plural),
-        }
-    }
-
-    fn batch_report_fail_msg(&self, err_ids: Vec<String>) -> String {
-        let plural = if err_ids.len() > 1 { "s" } else { "" };
-
-        match self.to_operation() {
-            Operation::Install => format!("❌ Fail to install {} image{}: {:?}", err_ids.len(), plural, err_ids),
-            Operation::Uninstall => format!("❌ Fail to uninstall {} image{}: {:?}", err_ids.len(), plural, err_ids),
-            Operation::Reinstall => format!("❌ Fail to reinstall {} image{}: {:?}", err_ids.len(), plural, err_ids),
-        }
-    }
-
-    fn print_batch_report(&self, report: (i32, Vec<String>)) -> Result<(), String> {
-        match report.clone() {
-            (ok_num, err_ids) if err_ids.is_empty() => {
-                println!("{}", self.batch_report_success_msg(ok_num));
-                Ok(())
-            }
-            (_, err_ids) => {
-                println!("{}", self.batch_report_fail_msg(err_ids));
-                Err(self.batch_report_msg(report))
-            }
-        }
-    }
-
-    fn success_fail_report(acc: (i32, Vec<String>), result: Result<ImageId, String>) -> (i32, Vec<String>) {
-        let add_element = |mut list: Vec<String>, element: String| -> Vec<String> {
-            list.push(element);
-            list
-        };
-
-        match result {
-            Ok(_) => (acc.0 + 1, acc.1),
-            Err(id_raw) => (acc.0, add_element(acc.1, id_raw)),
+                batch.execute(images, |id_raw| exec.reinstall(id_raw)),
         }
     }
 }
