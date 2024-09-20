b5c7799d3834865f01a1be745f64ef49c0c07b8e
Tobias Briones <tbriones@mathswe.com>
2024-09-20 16:52:57 -0600

    Set ops.math.software---mvp dev version 0.2.0-dev

--- ops.math.software---mvp/src/info.tsx
+++ ops.math.software---mvp/src/info.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // This file is part of https://github.com/mathswe-ops/mathswe-ops---mvp
 
-export const APP_SEMVER = "0.2.0";
+export const APP_SEMVER = "0.2.0-dev";
 export const APP_NAME = "Ops.Math.Software";
 export const APP_VERSION = `MVP v${ APP_SEMVER }`;
 export const APP_VERSION_LABEL = `${ APP_NAME } ${ APP_VERSION }`;
